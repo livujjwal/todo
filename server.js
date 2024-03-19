51fa8d1e2ec39cc0c30f3d97c15d95dbce8f53db
livujjwal <ujjwalsingh2112@gamil.com>
2024-03-19 19:30:38 +0530

    login auth completed

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const userValidation = require("./utils/userValidation");
+const { userValidation, isEmailRgex } = require("./utils/userValidation");
 const userModel = require("./Models/userModel");
+const bcrypt = require("bcrypt");
 require("dotenv").config();
 
 //variables
@@ -38,24 +39,38 @@ app.post("/signup", async (req, res) => {
   const { name, email, username, password } = req.body;
   try {
     await userValidation({ name, email, username, password });
+  } catch (error) {
+    return res.send({
+      status: 400,
+      message: "Validation fails",
+      error,
+    });
+  }
+  try {
+    const hashPassword = await bcrypt.hash(
+      password,
+      parseInt(process.env.SALT)
+    );
     const userObj = new userModel({
       name,
       email,
       username,
-      password,
+      password: hashPassword,
     });
     const userDb = await userObj.save();
-
     return res.send({
-      status: 200,
+      status: 201,
       message: "User Sign Up Successfully",
       data: userDb,
     });
   } catch (error) {
-    return res.send({
-      status: 400,
-      error,
-    });
+    if (error.keyValue.username || error.keyValue.email)
+      return res.send({
+        status: 400,
+        message: error.keyValue.username
+          ? "Username already taken"
+          : "Email already registered",
+      });
   }
   console.log(name, email, username, password);
 });
@@ -63,7 +78,38 @@ app.post("/signup", async (req, res) => {
 app.get("/login", (req, res) => {
   return res.render("loginPage");
 });
-app.post("/login", (req, res) => {
-  console.log(req.body);
-  return res.send("loginPage");
+app.post("/login", async (req, res) => {
+  const { loginId, password } = req.body;
+  let userDB;
+  try {
+    if (isEmailRgex({ email: loginId })) {
+      userDB = await userModel.findOne({ email: loginId });
+    } else {
+      userDB = await userModel.findOne({ username: loginId });
+    }
+    if (!userDB)
+      return res.send({
+        status: 400,
+        message: "Incorrect Login Credentials",
+      });
+    //compare password
+    const isPasswordMatch = await bcrypt.compare(password, userDB.password);
+    if (!isPasswordMatch)
+      return res.send({
+        status: 400,
+        message: "Incorrect Password",
+      });
+    console.log(userDB);
+    return res.send({
+      status: 200,
+      message: "User Login Successfully",
+      user: userDB,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.send({
+      status: 500,
+      message: "Server error",
+    });
+  }
 });
--- utils/userValidation.js
+++ utils/userValidation.js
@@ -1,4 +1,4 @@
-function isEmailRgex(email) {
+function isEmailRgex({ email }) {
   const isEmail =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i.test(
       email
@@ -10,7 +10,7 @@ const userValidation = ({ name, email, username, password }) => {
   return new Promise((resolve, reject) => {
     if (!email || !username || !password)
       reject("All input fields are required!");
-    if (!isEmailRgex(email)) reject("Email is invalid");
+    if (!isEmailRgex({ email })) reject("Email is invalid");
     if (username.length < 4 || username.length > 50)
       reject("Username must in between 4-50 character");
     if (password.length < 4 || password.length > 50)
@@ -19,4 +19,4 @@ const userValidation = ({ name, email, username, password }) => {
   });
 };
 
-module.exports = userValidation;
+module.exports = { userValidation, isEmailRgex };
